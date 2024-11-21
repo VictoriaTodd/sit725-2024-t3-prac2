a6d1ca6f4ebe285fe6978dc41d3447c91863baec
Victoria Todd <victoria.e.todd.98@gmail.com>
2024-11-21 14:36:14 +0100

    same functionality for subtraction

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.get("/addTwoNumbers", (req,res)=>{
     res.json({statuscode:200, data: result }); 
 });
 
+const subtractTwoNumber= (n1,n2) => {
+    return n1-n2;
+}
+app.get("/subtractTwoNumbers", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = subtractTwoNumber(n1,n2);
+    res.json({statuscode:200, data: result }); 
+});
+
 app.listen(port,()=>{
 console.log("App listening to: "+port)
 })
