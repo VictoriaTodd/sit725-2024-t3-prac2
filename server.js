a40e33de5fd8f472ca8eb5faed07bbf14f82f7a3
Victoria Todd <victoria.e.todd.98@gmail.com>
2024-11-21 14:38:27 +0100

    same functionality for multiply and divide

--- server.js
+++ server.js
@@ -24,6 +24,26 @@ app.get("/subtractTwoNumbers", (req,res)=>{
     res.json({statuscode:200, data: result }); 
 });
 
+const multiplyTwoNumber= (n1,n2) => {
+    return n1*n2;
+}
+app.get("/multiplyTwoNumbers", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = multiplyTwoNumber(n1,n2);
+    res.json({statuscode:200, data: result }); 
+});
+
+const divideTwoNumber= (n1,n2) => {
+    return n1/n2;
+}
+app.get("/divideTwoNumbers", (req,res)=>{
+    const n1= parseInt(req.query.n1);
+    const n2=parseInt(req.query.n2);
+    const result = divideTwoNumber(n1,n2);
+    res.json({statuscode:200, data: result }); 
+});
+
 app.listen(port,()=>{
 console.log("App listening to: "+port)
 })
